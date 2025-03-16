94cba90bdab51876de381d998ebd4cbbfbfcdde5
Denilson Linhares Feitosa Filho <denilson.filho@alaresinternet.com.br>
2025-03-16 13:31:33 -0300

    Criando modelo de teste.

--- src/dia05/model_churn/modeling/train/modeling.py
+++ src/dia05/model_churn/modeling/train/modeling.py
@@ -1,6 +1,9 @@
 import pandas as pd 
 import os
 import sqlalchemy
+from sklearn import tree
+from sklearn import model_selection
+from sklearn import metrics
 
 TRAIN_DIR = os.path.join( os.path.abspath('.'), 'src', 'dia05', 'model_churn', 'modeling', 'train')
 TRAIN_DIR = os.path.dirname( os.path.abspath( __file__ ) )
@@ -14,15 +17,33 @@ conn = engine.connect()
 abt = pd.read_sql_table('tb_abt_churn', conn)
 
 df_oot = abt[ abt['dt_ref'] == abt['dt_ref'].max() ].copy() # Filtrando base out of time
-
 df_abt = abt[ abt['dt_ref'] < abt['dt_ref'].max() ].copy()
 
 target = 'flag_churn'
-
 to_remove = ['dt_ref', 'seller_city', 'seller_state', 'seller_id', target] 
-
 features = df_abt.columns.tolist()
-
 for f in to_remove:
     features.remove( f )
     
+X_train, X_test, y_train, y_test = model_selection.train_test_split(df_abt[features],
+                                                                    df_abt[target],
+                                                                    test_size=0.2,
+                                                                    random_state=1992)
+
+clf = tree.DecisionTreeClassifier(min_samples_leaf=100)
+clf.fit(X_train, y_train)
+
+y_train_pred = clf.predict(X_train)
+y_train_prob = clf.predict_proba(X_train)
+print("/nAcurácia Treino:", metrics.accuracy_score(y_train, y_train_pred))
+print("AUC treino:", metrics.roc_auc_score(y_train, y_train_prob[:,1]))
+
+y_test_pred = clf.predict(X_test)
+y_test_prob = clf.predict_proba(X_test)
+print("/nAcurácia Teste:", metrics.accuracy_score(y_test, y_test_pred))
+print("AUC treino:", metrics.roc_auc_score(y_test, y_test_prob[:,1]))
+
+y_oot_pred = clf.predict(df_oot[features])
+y_oot_prob = clf.predict_proba(df_oot[features])
+print("/nAcurácia Out of Time:", metrics.accuracy_score(df_oot[target], y_oot_pred))
+print("AUC treino:", metrics.roc_auc_score(df_oot[target], y_oot_prob[:,1]))
