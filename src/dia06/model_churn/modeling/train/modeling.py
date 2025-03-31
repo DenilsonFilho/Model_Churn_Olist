793ff2391fbfcf1b080756a0f6aa49cb5fb468a1
Denilson Linhares Feitosa Filho <denilson.filho@alaresinternet.com.br>
2025-03-31 14:20:09 -0300

    Criando o primeiro classificador!

--- src/dia06/model_churn/modeling/train/modeling.py
+++ src/dia06/model_churn/modeling/train/modeling.py
@@ -4,6 +4,7 @@ import sqlalchemy
 from sklearn import tree
 from sklearn import model_selection
 from sklearn import metrics
+from sklearn import preprocessing
 
 TRAIN_DIR = os.path.join( os.path.abspath('.'), 'src', 'dia05', 'model_churn', 'modeling', 'train')
 TRAIN_DIR = os.path.dirname( os.path.abspath( __file__ ) )
@@ -17,13 +18,16 @@ conn = engine.connect()
 abt = pd.read_sql_table('tb_abt_churn', conn)
 
 df_oot = abt[ abt['dt_ref'] == abt['dt_ref'].max() ].copy() # Filtrando base out of time
-df_abt = abt[ abt['dt_ref'] < abt['dt_ref'].max() ].copy()
+df_abt = abt[ abt['dt_ref'] < abt['dt_ref'].max() ].copy() # Filtrando base ABT
 
 target = 'flag_churn'
 to_remove = ['dt_ref', 'seller_city', 'seller_id', target] 
 features = df_abt.columns.tolist()
 for f in to_remove:
     features.remove( f )
+    
+cat_features = df_abt[features].dtypes[ df_abt[features].dtypes == 'object' ].index.tolist()
+num_features = list( set( features ) - set( cat_features ) )
     
 X = df_abt[features]    
 y = df_abt[target]
@@ -33,20 +37,53 @@ X_train, X_test, y_train, y_test = model_selection.train_test_split(X,
                                                                     test_size=0.2,
                                                                     random_state=1992)
 
+X_train.reset_index(drop=True, inplace=True)
+X_test.reset_index(drop=True, inplace=True)
+
+onehot = preprocessing.OneHotEncoder(sparse_output=False, handle_unknown='ignore')
+onehot.fit( X_train[cat_features] )
+
+onehot_df = pd.DataFrame(onehot.transform( X_train[cat_features] ), 
+             columns=onehot.get_feature_names_out(cat_features))
+
+df_train = pd.concat([X_train[num_features], onehot_df], axis=1)
+df_train= df_train.rename(str,axis="columns") 
+features_fit = df_train.columns.tolist()
+
+# Modelo
 clf = tree.DecisionTreeClassifier(min_samples_leaf=100)
-clf.fit(X_train, y_train)
+clf.fit( df_train[features_fit], y_train )
 
-y_train_pred = clf.predict(X_train)
-y_train_prob = clf.predict_proba(X_train)
-print("/nAcurácia Treino:", metrics.accuracy_score(y_train, y_train_pred))
-print("AUC treino:", metrics.roc_auc_score(y_train, y_train_prob[:,1]))
+pd.Series(clf.feature_importances_, index=df_train.columns).sort_values(ascending=False)[:10]
 
-y_test_pred = clf.predict(X_test)
-y_test_prob = clf.predict_proba(X_test)
-print("/nAcurácia Teste:", metrics.accuracy_score(y_test, y_test_pred))
-print("AUC treino:", metrics.roc_auc_score(y_test, y_test_prob[:,1]))
+# Analise na base de treino
 
-y_oot_pred = clf.predict(df_oot[features])
-y_oot_prob = clf.predict_proba(df_oot[features])
-print("/nAcurácia Out of Time:", metrics.accuracy_score(df_oot[target], y_oot_pred))
-print("AUC treino:", metrics.roc_auc_score(df_oot[target], y_oot_prob[:,1]))
+y_train_pred = clf.predict(df_train)
+print('Base Treino:', metrics.accuracy_score(y_train, y_train_pred))
+
+# Analise na base de teste
+onehot_df_test = pd.DataFrame(onehot.transform( X_test[cat_features] ), 
+             columns=onehot.get_feature_names_out(cat_features))
+df_predict = pd.concat([X_test[num_features], onehot_df_test], axis=1)
+df_predict = df_predict.rename(str, axis='columns')
+y_test_pred = clf.predict(df_predict)
+print('Base test:', metrics.accuracy_score(y_test, y_test_pred))
+
+# Analise na base de out of time
+df_oot.reset_index(drop=True, inplace=True)
+onehot_df_oot = pd.DataFrame(onehot.transform( df_oot[cat_features] ), 
+             columns=onehot.get_feature_names_out(cat_features))
+df_predict_oot = pd.concat([df_oot[num_features], onehot_df_oot], axis=1)
+df_predict_oot = df_predict_oot.rename(str, axis='columns')
+oot_pred = clf.predict(df_predict_oot)
+print('Base out of time:', metrics.accuracy_score(df_oot[target], oot_pred))
+
+# Fazendo o predict
+df_abt_onehot = pd.DataFrame(onehot.transform( abt[cat_features] ), 
+             columns=onehot.get_feature_names_out(cat_features))
+df_abt_predict = pd.concat([abt[num_features], df_abt_onehot], axis=1)
+df_abt_predict = df_abt_predict.rename(str, axis='columns')
+
+probs = clf.predict_proba(df_abt_predict)
+
+abt['score_churn'] = probs = clf.predict_proba(df_abt_predict)[:,1]
